bb34dc845f53ace1c2220fcf11ea7b3664a946d6
Anusha Hegde <ahegde@newrelic.com>
2024-12-12 14:59:52 +0530

    fixes

--- workflow-scripts/update_lambda_template.py
+++ workflow-scripts/update_lambda_template.py
@@ -1,24 +1,30 @@
-import re
+import yaml
+
+def unknown_constructor(loader, tag_suffix, node):
+    if isinstance(node, yaml.ScalarNode):
+        return loader.construct_scalar(node)
+    elif isinstance(node, yaml.SequenceNode):
+        return loader.construct_sequence(node)
+    elif isinstance(node, yaml.MappingNode):
+        return loader.construct_mapping(node)
+    return None
 
 file_path = 'lambda-template.yaml'
 
-with open(file_path, 'r') as file:
-    content = file.read()
+with open(file_path, 'r') as stream:
+    try:
+        yaml.add_multi_constructor('!', unknown_constructor, Loader=yaml.FullLoader)
+        data = yaml.load(stream, Loader=yaml.FullLoader)
+    except yaml.YAMLError as exc:
+        print(exc)
 
-# CodeUri: src/ pattern is identified and replaced with production s3 bucket arn
-pattern = re.compile(
-    r'CodeUri:\s*src/\s*'
-)
-
-replacement = (
-    "CodeUri:\n"
-    "        Bucket: !FindInMap [ RegionToS3Bucket, !Ref 'AWS::Region', BucketArn ]\n"
-    "        Key: 'new-relic-log-forwarder-folder/new-relic-log-forwarder.zip'\n"
-)
-
-new_content = pattern.sub(replacement, content)
+code_uri_path = data.get('Resources', {}).get('NewRelicLogsServerlessLogForwarder', {}).get('Properties', {})
+code_uri_path['CodeUri'] = {
+    'Bucket': "!FindInMap [ RegionToS3Bucket, !Ref 'AWS::Region', BucketArn ]",
+    'Key': 'new-relic-log-forwarder-folder/new-relic-log-forwarder.zip'
+}
 
 with open(file_path, 'w') as file:
-    file.write(new_content)
+    yaml.dump(data, file, default_flow_style=False)
 
 print("Template file updated successfully.")
