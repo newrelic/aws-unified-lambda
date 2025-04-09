04e98f2452e4eafc9e08ad544792feb1d5855236
Anusha Hegde <ahegde@newrelic.com>
2025-04-09 14:16:08 +0530

    delete stack pr

--- e2e-tests/lambda-cloudwatch-trigger.sh
+++ e2e-tests/lambda-cloudwatch-trigger.sh
@@ -6,85 +6,14 @@ source common-scripts/logs-scripts.sh
 source common-scripts/test-configs.cfg
 
 test_logs_with_filter_pattern() {
-cat <<EOF > cloudwatch-parameter.json
-'[{"LogGroupName":"$LOG_GROUP_NAME","FilterPattern":"$LOG_GROUP_FILTER_PATTERN"}]'
-EOF
-
-cat <<EOF > common_attribute.json
-'[{"AttributeName":"$CUSTOM_ATTRIBUTE_KEY","AttributeValue":"$CUSTOM_ATTRIBUTE_VALUE"}]'
-EOF
-COMMON_ATTRIBUTES=$(<common_attribute.json)
-
-  LOG_GROUP_NAMES=$(<cloudwatch-parameter.json)
-  echo "Testing for log group configuration JSON: $(<cloudwatch-parameter.json)"
-
-  log_message_with_filter_pattern=$(create_log_message "$LOG_MESSAGE_CLOUDWATCH" "$LOG_GROUP_FILTER_PATTERN")
-  log_message_without_filter_pattern=$(create_log_message "$LOG_MESSAGE_CLOUDWATCH" "")
-
-  deploy_cloudwatch_trigger_stack "$CLOUDWATCH_FILTER_PATTERN_CASE" "false"  "$LOG_GROUP_NAMES" "$COMMON_ATTRIBUTES"
-  validate_stack_deployment_status "$CLOUDWATCH_FILTER_PATTERN_CASE"
-  validate_lambda_subscription_created "$CLOUDWATCH_FILTER_PATTERN_CASE" "$LOG_GROUP_NAME" "$LOG_GROUP_FILTER_PATTERN"
-
-  # test that when logs with matching filter pattern are present, they are sent to New Relic
-  create_cloudwatch_log_event "$LOG_GROUP_NAME" "$LOG_STREAM_NAME" "$log_message_with_filter_pattern"
-  validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$ATTRIBUTE_KEY_CLOUDWATCH" "$LOG_STREAM_NAME" "$log_message_with_filter_pattern"
-
-  # test that when logs without matching filter pattern are present, they are not sent to New Relic
-  create_cloudwatch_log_event "$LOG_GROUP_NAME" "$LOG_STREAM_NAME" "$log_message_without_filter_pattern"
-  validate_logs_not_present "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$ATTRIBUTE_KEY_CLOUDWATCH" "$LOG_STREAM_NAME" "$log_message_without_filter_pattern"
-
   delete_stack "$CLOUDWATCH_FILTER_PATTERN_CASE"
-}
-
-test_logs_for_secret_manager() {
-cat <<EOF > cloudwatch-parameter.json
-'[{"LogGroupName":"$LOG_GROUP_NAME_SECRET_MANAGER","FilterPattern":"$LOG_GROUP_FILTER_PATTERN"}]'
-EOF
-
-cat <<EOF > common_attribute.json
-'[{"AttributeName":"$CUSTOM_ATTRIBUTE_KEY","AttributeValue":"$CUSTOM_ATTRIBUTE_VALUE"}]'
-EOF
-COMMON_ATTRIBUTES=$(<common_attribute.json)
-
-  LOG_GROUP_NAMES=$(<cloudwatch-parameter.json)
-  echo "Testing for log group configuration JSON: $(<cloudwatch-parameter.json)"
-
-  log_message=$(create_log_message "$LOG_MESSAGE_CLOUDWATCH" "$LOG_GROUP_FILTER_PATTERN")
-
-  deploy_cloudwatch_trigger_stack "$CLOUDWATCH_SECRET_MANAGER_CASE" "true" "$LOG_GROUP_NAMES" "$COMMON_ATTRIBUTES"
-  validate_stack_deployment_status "$CLOUDWATCH_SECRET_MANAGER_CASE"
-  validate_lambda_subscription_created "$CLOUDWATCH_SECRET_MANAGER_CASE" "$LOG_GROUP_NAME_SECRET_MANAGER" "$LOG_GROUP_FILTER_PATTERN"
-  create_cloudwatch_log_event "$LOG_GROUP_NAME_SECRET_MANAGER" "$LOG_STREAM_NAME" "$log_message"
-  validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$ATTRIBUTE_KEY_CLOUDWATCH" "$LOG_STREAM_NAME" "$log_message"
   delete_stack "$CLOUDWATCH_SECRET_MANAGER_CASE"
-}
-
-test_logs_for_invalid_log_group() {
-cat <<EOF > cloudwatch-parameter.json
-'[{"LogGroupName":"$LOG_GROUP_NAME_INVALID","FilterPattern":"$LOG_GROUP_FILTER_PATTERN"}]'
-EOF
-
-  LOG_GROUP_NAMES=$(<cloudwatch-parameter.json)
-  echo "Testing for log group configuration JSON: $(<cloudwatch-parameter.json)"
-
-  deploy_cloudwatch_trigger_stack "$CLOUDWATCH_INVALID_LOG_GROUP_CASE" "false" "$LOG_GROUP_NAMES" "''"
-  validate_stack_deployment_status "$CLOUDWATCH_INVALID_LOG_GROUP_CASE"
-
-  # validate that lambda subscription is not created
-  validate_lambda_subscription_not_created "$CLOUDWATCH_INVALID_LOG_GROUP_CASE" "$LOG_GROUP_NAME_INVALID" "$LOG_GROUP_FILTER_PATTERN"
-
   delete_stack "$CLOUDWATCH_INVALID_LOG_GROUP_CASE"
 }
 
 case $1 in
   test_logs_with_filter_pattern)
     test_logs_with_filter_pattern
-    ;;
-  test_logs_for_secret_manager)
-    test_logs_for_secret_manager
-    ;;
-  test_logs_for_invalid_log_group)
-    test_logs_for_invalid_log_group
     ;;
   *)
   echo "Invalid test case specified."
